2dca697daa21e47b28f4a13095f0ddb926644e69
пгыуgusevskiy <vlgu@mail.ru>
2024-08-10 23:01:09 +0300

    Запрос к API Геокодера работает,json прилетает

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Flask_Project
 Модуль Flask Blueprint
+
+
+#### Используемые резурсы
+
+[API Геокодера YAndex](https://yandex.ru/dev/geocode/doc/ru/request)
+[Flask](https://flask.palletsprojects.com/en/2.0.x/)
---
+++ my_test_my/main.py
@@ -0,0 +1,32 @@
+import requests
+import sys
+import os
+import json
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils.settings import settings
+
+
+def main():
+    apikey = settings.my_env.apikey
+    geocode = "бульвар Мухаммед Бин Рашид 1"
+    lang = "ru_RU"
+    
+
+    url = f"https://geocode-maps.yandex.ru/1.x?apikey={apikey}&geocode={geocode}&lang={lang}&format=json"
+
+
+    response = requests.get(url=url)
+
+    if response.status_code == 200:
+        data_json = response.json()
+        print(data_json)
+        with open(f"C:\\DEV_python\\Test_Task\\Logik_Flask\\Flask_Project\\files_json\\{geocode}.json", "w", encoding="utf-8") as json_file:
+            json.dump(data_json, json_file, ensure_ascii=False, indent=4)
+    else:
+        print("Ошибка", response.status_code)
+
+
+if __name__ == '__main__':
+    main()
