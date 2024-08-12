df70b342c43e5c982294e985a4e38020e72684a0
пгыуgusevskiy <vlgu@mail.ru>
2024-08-13 00:05:27 +0300

    Save

--- blueprints/address/address.py
+++ blueprints/address/address.py
@@ -20,18 +20,16 @@ def post_address():
         curl --request POST --header "Content-Type: application/json" --data '{"address": "Москва, Центральный административный округ, район Хамовники, 461-й квартал"}' http://localhost:5000/address/post_address
     Blueprint:
         Часть адреса: "address"
-    Возвращает:
+    Возвращает string:
         "До адреса 'Россия Московская область Мытищи Новомытищинский проспект 19А' растояние от МКАДА = 2.85 км"
     """
     try:
         # Декодируем кирилицу в cp1251 (utf8 не раскодирует тк разрабатываю на windows)
         raw_data = request.data.decode('cp1251')
-        logger.debug(f"Decoded data: {raw_data}")
-
+        # Строку конвертируем в json
         json_data = json.loads(raw_data)
+        # Из json получаем адрес
         json_obj = json_data.get('address')
-        # Собираем полученый json в одно предложение.
-        # text_address = f"{json_obj.get('country')} {json_obj.get('region')} {json_obj.get('city')} {json_obj.get('street')} {json_obj.get('house')}"
         # Отправляем на API Геокодера.
         geo_object = api_geocode(json_obj)
         # Из полученного json берем только Point среднию точку указанного адреса.
@@ -46,4 +44,4 @@ def post_address():
         # Ловим общие ошибки по Blueprint.
         logger.error(f"Ошибка Blueprint {str(e)}")
         # Возвращаем для отображении в результате в форме.
-        return jsonify({"error": "Internal server error"})
+        return jsonify({"message": "Internal server error"})
