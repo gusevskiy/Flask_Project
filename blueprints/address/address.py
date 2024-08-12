69821cbb2b6a40abca7f890ac0b7768efe704d4e
пгыуgusevskiy <vlgu@mail.ru>
2024-08-12 23:28:47 +0300

    настроил curl запрос, была проблемма с кодировкой кирилицы

--- blueprints/address/address.py
+++ blueprints/address/address.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from loguru import logger
-
+import json
 from utils.log_config import setup_logger
 
 from .utils_address.geocode import api_geocode
@@ -17,15 +17,19 @@ def post_address():
     """
     Обработка запросов POST к маршруту /post_address.
     Принимает JSON:
-        {'country': 'Россия', 'region': 'Nizhniy Novgorod', 'city': 'Кстово г, Кстовский р-н, Нижегородская обл', 'street': 'Парковая у', 'house': '1'}
-        http://localhost:5000/address/post_address
+        curl --request POST --header "Content-Type: application/json" --data '{"address": "Москва, Центральный административный округ, район Хамовники, 461-й квартал"}' http://localhost:5000/address/post_address
     Blueprint:
         Часть адреса: "address"
     Возвращает:
         "До адреса 'Россия Московская область Мытищи Новомытищинский проспект 19А' растояние от МКАДА = 2.85 км"
     """
     try:
-        json_obj = request.json.get('address')
+        # Декодируем кирилицу в cp1251 (utf8 не раскодирует тк разрабатываю на windows)
+        raw_data = request.data.decode('cp1251')
+        logger.debug(f"Decoded data: {raw_data}")
+
+        json_data = json.loads(raw_data)
+        json_obj = json_data.get('address')
         # Собираем полученый json в одно предложение.
         # text_address = f"{json_obj.get('country')} {json_obj.get('region')} {json_obj.get('city')} {json_obj.get('street')} {json_obj.get('house')}"
         # Отправляем на API Геокодера.
